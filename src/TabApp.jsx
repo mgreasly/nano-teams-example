b88b457880c305428d87f0a81edb403b8a2820a9
Marcus Greasly <mgreasly@hotmail.com>
2022-06-27 09:42:35 +1000

    Updated teams js code

--- src/TabApp.jsx
+++ src/TabApp.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import * as teams from '@microsoft/teams-js';
 import Links from './Links';
 
 export default () => {
     console.log("initialise...");
+    const [context, setContext] = useState({});
     teams.app.initialize();
-    var data = teams.app.getContext();
+    app.getContext().then((context) => {
+        console.log("context... ");
+        console.log(context);
+        setContext(context);
+    });
 
     return (
         <>
             <h1>TAB:</h1>
             <div>Context</div>
             <div>
-                <pre>{JSON.stringify(data, null, 2)}</pre>
+                <pre>{JSON.stringify(context, null, 2)}</pre>
             </div>
             <Links />
         </>
